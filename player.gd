e58f056b8c2740bebdcf132233ffb3d0b3b99b88
RadicalPipo <eduardodlcmz@gmail.com>
2025-11-25 18:54:11 -0500

    animaciones de caminar e idle

--- player.gd
+++ player.gd
@@ -1,25 +1,47 @@
 extends CharacterBody2D
 
-@export var speed: float = 120.0
+@export var speed := 90
 
-func _physics_process(delta: float) -> void:
-	var dir = Vector2.ZERO
+var direction := Vector2.ZERO
+var last_direction := "down"
+@onready var anim := $AnimatedSprite2D
 
-	if Input.is_action_pressed("ui_right"):
-		dir.x += 1
-	if Input.is_action_pressed("ui_left"):
-		dir.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		dir.y += 1
-	if Input.is_action_pressed("ui_up"):
-		dir.y -= 1
+func _physics_process(delta):
+	direction = Vector2.ZERO
 
-	dir = dir.normalized()
-	velocity = dir * speed
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+
+	
+	if direction != Vector2.ZERO:
+		velocity = direction.normalized() * speed
+		update_animation(direction)
+	else:
+		velocity = Vector2.ZERO
+		anim.play("idle_" + last_direction)
 
 	move_and_slide()
 
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-
-func _ready() -> void:
-	anim.play("idle")
+func update_animation(dir: Vector2):
+	if abs(dir.x) > abs(dir.y):
+		
+		if dir.x > 0:
+			anim.play("walk_right")
+			last_direction = "right"
+		else:
+			anim.play("walk_left")
+			last_direction = "left"
+	else:
+		
+		if dir.y > 0:
+			anim.play("walk_down")
+			last_direction = "down"
+		else:
+			anim.play("walk_up")
+			last_direction = "up"
